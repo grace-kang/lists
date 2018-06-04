a852c76d46e529c9b04cff40c8d1bad22999cbab
Grace Kang <grace@stembolt.com>
2018-06-04 16:30:17 -0700

    removing unneccesary controllers

--- apps/web/controllers/items/new.rb
+++
@@ -1,7 +0,0 @@
-module Web::Controllers::Items
-  class New
-    include Web::Action
-
-    def call(params); end
-  end
-end
--- apps/web/controllers/lists/new.rb
+++
@@ -1,7 +0,0 @@
-module Web::Controllers::Lists
-  class New
-    include Web::Action
-
-    def call(params); end
-  end
-end
