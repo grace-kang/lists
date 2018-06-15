e000a25c291422e5c3d9d36a3dc32e43c16db658
Grace Kang <grace@stembolt.com>
2018-06-15 09:42:19 -0700

    adding animation speed to lists

--- apps/web/assets/javascripts/drag_drop_lists.js
+++ apps/web/assets/javascripts/drag_drop_lists.js
@@ -2,6 +2,7 @@ var lists = document.getElementsByName('list');
 
 for (var i = 0; i < lists.length; i++) {
 	var sort = Sortable.create(lists[i], {
+		animation: 150,
 		onUpdate: function (evt/**Event*/){
 			var item = evt.item;
 			console.log("onUpdate");
