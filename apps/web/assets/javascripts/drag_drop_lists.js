8059e3eac54299b0f53736ac9b7700fcd4b217ae
Grace Kang <grace@MSI-Trident3.localdomain>
2018-06-14 18:57:40 -0700

    switched to sortablejs

--- apps/web/assets/javascripts/drag_drop_lists.js
+++ apps/web/assets/javascripts/drag_drop_lists.js
@@ -1,18 +1,4 @@
-export default function Flexbox() {
-  const containers = document.getElementsByName('list');
-  console.log(containers);
+  var lists = document.getElementsByName('list');
+  console.log(lists);
 
-  if (containers.length === 0) {
-    return false;
-  }
-
-  const swappable = new Swappable(containers, {
-    draggable: '.Block--isDraggable',
-    mirror: {
-      constrainDimensions: true,
-    },
-    plugins: [Plugins.ResizeMirror],
-  });
-
-  return swappable;
-}
+  Sortable.create(list);
